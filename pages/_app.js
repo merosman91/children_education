5a42656f958fe95d9e71a2cfaac449f3af3c18ca
merosman91 <merghanigasimosman@gmail.com>
2025-12-02 15:58:07 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,37 @@
-// pages/_app.js
-// هذا الملف ضروري لتهيئة صفحات Next.js بشكل صحيح
-export default function MyApp({ Component, pageProps }) {
+import Head from 'next/head';
+
+function MyApp({ Component, pageProps }) {
   return (
     <>
-      {/* تطبيق تنسيقات بسيطة عامة لإزالة الهوامش الافتراضية */}
+      <Head>
+        <title>مدرسة الإخلاص | أبودوم</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* استيراد خط Cairo من Google Fonts */}
+        <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;700&display=swap" rel="stylesheet" />
+      </Head>
+
+      {/* تطبيق التنسيقات العامة */}
       <style jsx global>{`
-        body {
+        * {
+          box-sizing: border-box;
           margin: 0;
           padding: 0;
-          font-family: sans-serif;
+        }
+        body {
+          font-family: 'Cairo', sans-serif;
+          direction: rtl;
+          background-color: #f5f7fa;
+          color: #333;
+        }
+        a {
+          text-decoration: none;
+          color: inherit;
         }
       `}</style>
+
       <Component {...pageProps} />
     </>
   );
 }
+
+export default MyApp;
